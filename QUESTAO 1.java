eed3faa060bf282e581408a7051f829410ed36f6
TheusGeto <matheusdbrwolf2021@gmail.com>
2024-10-07 18:56:15 -0300

    Update QUESTAO 1.java

--- QUESTAO 1.java
+++ QUESTAO 1.java
@@ -2,34 +2,34 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Inicializa o vetor de 20 elementos
+        // Inicia o vetor
         int[] vetor = new int[20];
         int contadorImpares = 0;
         
-        // Scanner para leitura dos valores
+        // Scanner que le os numeros
         Scanner scanner = new Scanner(System.in);
         
-        // Leitura dos 20 elementos e preenchimento do vetor
+        // Leitura dos 20 numeros e coloca no vetor
         System.out.println("Digite 20 números inteiros:");
         for (int i = 0; i < 20; i++) {
             vetor[i] = scanner.nextInt();
         }
         
-        // Mostra os elementos do vetor
+        // Mostra o conteudo do vetor
         System.out.println("Valores inseridos no vetor:");
         for (int i = 0; i < 20; i++) {
             System.out.print(vetor[i] + " ");
         }
         System.out.println();
         
-        // Conta quantos números ímpares existem no vetor
+        // Conta quantos números ímpares estao no vetor
         for (int i = 0; i < 20; i++) {
             if (vetor[i] % 2 != 0) {
                 contadorImpares++;
             }
         }
         
-        // Exibe o número de valores ímpares
+        // Exibe a quantidade de ímpares
         System.out.println("Quantidade de valores ímpares: " + contadorImpares);
         
         scanner.close();
