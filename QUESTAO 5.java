9d6560c2f24784ea2bd3f0a53661d8a35cf111ba
TheusGeto <matheusdbrwolf2021@gmail.com>
2024-10-07 19:51:02 -0300

    Create QUESTAO 5.java

---
+++ QUESTAO 5.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Inicial o vetor
+        int[] vetor = new int[10];
+        
+        // le os numeros
+        Scanner scanner = new Scanner(System.in);
+        
+        // Le os numeros
+        System.out.println("Digite 10 números inteiros:");
+        for (int i = 0; i < 10; i++) {
+            vetor[i] = scanner.nextInt();
+        }
+        
+        // Transforma a ordem em decrecente
+        Arrays.sort(vetor);
+        
+        // Exibindo numeros
+        System.out.println(" numeros em ordem decrescente:");
+        for (int i = 9; i >= 0; i--) {
+            System.out.print(vetor[i] + " ");
+        }
+        
+        scanner.close();
+    }
+}
